a9c4f89a115559c170bc56d3bc1a4c19da7c3511
ilyavpakhomov <40854452+ilyavpakhomov@users.noreply.github.com>
2022-02-20 22:19:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ for (let i = 0; i < SQUARES_NUMBER; i++){
     const square = document.createElement('div')
     square.classList.add('square')
     square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('touchstart', () => setColor(square))
     square.addEventListener('mouseleave', () => removeColor(square))
+    square.addEventListener('touchend', () => removeColor(square))
     board.append(square)
 }
 
